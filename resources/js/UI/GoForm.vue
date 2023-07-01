8eb413bf930afc04db7b16a510b7fcee3bc93f67
weasel-v-dev <madara.avv@gmail.com>
2023-07-01 19:47:13 +0300

    Added new fields in form

--- resources/js/UI/GoForm.vue
+++ resources/js/UI/GoForm.vue
@@ -1,115 +1,262 @@
 <template>
-    <div class="suka">
-        <validation-observer
+    <validation-observer
             ref="observer"
             v-slot="{ invalid }"
         >
             <form @submit.prevent="submit">
-                <validation-provider
-                    v-slot="{ errors }"
-                    name="DealName"
-                    rules="required|max:50"
-                >
-                    <v-text-field
-                        v-model="dealName"
-                        :error-messages="errors"
-                        label="Deal Name"
-                        required
-                    ></v-text-field>
-                </validation-provider>
-                <validation-provider
-                    v-slot="{ errors }"
-                    name="Stage"
-                    rules="required|max:50"
-                >
-                    <v-text-field
-                        v-model="stage"
-                        :error-messages="errors"
-                        label="Stage"
-                        required
-                    ></v-text-field>
-                </validation-provider>
-                <validation-provider
-                    v-slot="{ errors }"
-                    name="AccountName"
-                    rules="required|max:50"
-                >
-                    <v-text-field
-                        v-model="accountName"
-                        :error-messages="errors"
-                        label="Account Name"
-                        required
-                    ></v-text-field>
-                </validation-provider>
-                <validation-provider
-                    v-slot="{ errors }"
-                    name="Phone"
-                    :rules="{
-          required: true,
-          max: 15,
-          regex: '^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$'
-        }"
-                >
-                    <v-text-field
-                        v-model="phone"
-                        :error-messages="errors"
-                        label="Account Phone"
-                        required
-                    ></v-text-field>
-                </validation-provider>
-                <validation-provider
-                    v-slot="{ errors }"
-                    name="Email"
-                    rules="required|email"
-                >
-                    <v-text-field
-                        v-model="email"
-                        :error-messages="errors"
-                        label="E-mail"
-                        required
-                    ></v-text-field>
-                </validation-provider>
-                <validation-provider
-                    v-slot="{ errors }"
-                    name="Website"
-                    :rules="{
+                <div class="row">
+                    <div class="col-md-6">
+                        <h3><b>Deal information</b></h3>
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="DealName"
+                            rules="required|max:50"
+                        >
+                            <v-text-field
+                                v-model="dealName"
+                                :error-messages="errors"
+                                label="Deal Name"
+                                required
+                            ></v-text-field>
+                        </validation-provider>
+
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="DealOwner"
+                            rules="required|max:50"
+                        >
+                            <v-text-field
+                                v-model="dealOwner"
+                                :error-messages="errors"
+                                label="Deal Owner"
+                                required
+                            ></v-text-field>
+                        </validation-provider>
+
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="Amount"
+                            rules="required|max:50"
+                        >
+                            <v-text-field
+                                v-model="amount"
+                                :error-messages="errors"
+                                label="Amount"
+                            ></v-text-field>
+                        </validation-provider>
+
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="Stage"
+                            rules="required|max:50"
+                        >
+                            <v-text-field
+                                v-model="stage"
+                                :error-messages="errors"
+                                label="Stage"
+                                required
+                            ></v-text-field>
+                        </validation-provider>
+
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="closingDate"
+                            rules="required"
+                            class="position-relative"
+
+                        >
+                            <v-text-field
+                                v-model="closingDate"
+                                :error-messages="errors"
+                                label="Closing Date"
+                                required
+                                @click="triggerModalDate = true"
+                            ></v-text-field>
+                            <div class="position-absolute z-1 start-0">
+                                <v-date-picker v-if="triggerModalDate" @change="triggerModalDate = false"  v-model="closingDate"></v-date-picker>
+                            </div>
+                        </validation-provider>
+
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="Probability"
+                            rules="required|max:50"
+                        >
+                            <v-text-field
+                                v-model="probability"
+                                :error-messages="errors"
+                                label="Probability"
+                                required
+                            ></v-text-field>
+                        </validation-provider>
+
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="dealDescription"
+                            rules="required|max:500"
+                        >
+                            <v-textarea
+                                v-model="dealDescription"
+                                :error-messages="errors"
+                                label="Deal description"
+                                required
+                            ></v-textarea>
+                        </validation-provider>
+                    </div>
+                    <div class="col-md-6">
+                        <h3 class="mt-3 mt-md-0 "><b>Account information</b></h3>
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="AccountName"
+                            rules="required|max:50"
+                        >
+                            <v-text-field
+                                v-model="accountName"
+                                :error-messages="errors"
+                                label="Account Name"
+                                required
+                            ></v-text-field>
+                        </validation-provider>
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="AccountOwner"
+                            rules="required|max:50"
+                        >
+                            <v-text-field
+                                v-model="accountOwner"
+                                :error-messages="errors"
+                                label="Account owner"
+                                required
+                            ></v-text-field>
+                        </validation-provider>
+
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="accountType"
+                            rules="required"
+                        >
+                            <v-select
+                                v-model="accountType"
+                                :items="['item 1','item 2']"
+                                :error-messages="errors"
+                                label="Account type"
+                                data-vv-name="select"
+                                required
+                            ></v-select>
+                        </validation-provider>
+
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="BillingAddress"
+                            rules="required|max:50"
+                        >
+                            <v-text-field
+                                v-model="billingAddress"
+                                :error-messages="errors"
+                                label="Billing address"
+                                required
+                            ></v-text-field>
+                        </validation-provider>
+
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="ShippingAddress"
+                            rules="required|max:50"
+                        >
+                            <v-text-field
+                                v-model="shippingAddress"
+                                :error-messages="errors"
+                                label="Shipping address"
+                                required
+                            ></v-text-field>
+                        </validation-provider>
+
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="Phone"
+                            :rules="{
+                      required: true,
+                      max: 18,
+                      regex: '^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$'
+                    }"
+                        >
+                            <v-text-field
+                                v-model="phone"
+                                :error-messages="errors"
+                                label="Account Phone"
+                                required
+                            ></v-text-field>
+                        </validation-provider>
+
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="Email"
+                            rules="required|email"
+                        >
+                            <v-text-field
+                                v-model="email"
+                                :error-messages="errors"
+                                label="E-mail"
+                                required
+                            ></v-text-field>
+                        </validation-provider>
+
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="Website"
+                            :rules="{
                       required: true,
                       regex: 'https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)'
                     }">
-                        <v-text-field
-                            v-model="website"
-                            :error-messages="errors"
-                            label="Account Website"
-                            required
-                        ></v-text-field>
-                </validation-provider>
-                <validation-provider
-                    v-slot="{ errors }"
-                    rules="required"
-                    name="checkbox"
-                >
-                    <v-checkbox
-                        v-model="checkbox"
-                        :error-messages="errors"
-                        value="1"
-                        label="Option"
-                        type="checkbox"
-                        required
-                    ></v-checkbox>
-                </validation-provider>
-                <v-btn
-                    class="mr-4"
-                    type="submit"
-                    :disabled="invalid"
-                >
-                    submit
-                </v-btn>
-                <v-btn @click="clear">
-                    clear
-                </v-btn>
+                            <v-text-field
+                                v-model="website"
+                                :error-messages="errors"
+                                label="Account Website"
+                                required
+                            ></v-text-field>
+                        </validation-provider>
+
+                        <validation-provider
+                            v-slot="{ errors }"
+                            name="industry"
+                            rules="required|max:50"
+                        >
+                            <v-text-field
+                                v-model="industry"
+                                :error-messages="errors"
+                                label="Industry"
+                                required
+                            ></v-text-field>
+                        </validation-provider>
+
+                        <validation-provider
+                            v-slot="{ errors }"
+                            rules="required"
+                            name="checkbox"
+                        >
+                            <v-checkbox
+                                v-model="checkbox"
+                                :error-messages="errors"
+                                value="1"
+                                label="Option"
+                                type="checkbox"
+                                required
+                            ></v-checkbox>
+                        </validation-provider>
+                        <v-btn
+                            class="mr-4"
+                            type="submit"
+                            :disabled="invalid"
+                        >
+                            submit
+                        </v-btn>
+                        <v-btn @click="clear">
+                            clear
+                        </v-btn>
+                    </div>
+                </div>
             </form>
         </validation-observer>
-    </div>
 </template>
 
 <script>
@@ -149,42 +296,84 @@ export default {
     },
     data: () => ({
         dealName: '',
+        dealOwner: '',
+
+        amount: '',
+        stage: '',
+        closingDate: '',
+        probability: '',
+        dealDescription: '',
+
+
         accountName: '',
+        accountOwner: '',
+        accountType: '',
+        billingAddress: '',
+        shippingAddress: '',
         phone: '',
         email: '',
         website: '',
+        industry: '',
+
+
         checkbox: null,
-        stage: ''
+        triggerModalDate: false
     }),
 
     methods: {
         submit () {
-            console.log(this.$refs.observer.validate())
             this.send();
         },
         clear () {
             this.dealName = ''
+
+            this.dealOwner = ''
+            this.amount = ''
+            this.stage = ''
+            this.closingDate = ''
+            this.probability = ''
+            this.dealDescription = ''
+
+
             this.accountName = ''
+            this.accountOwner = ''
+            this.accountType = ''
+            this.billingAddress = ''
+            this.shippingAddress = ''
             this.phone = ''
             this.email = ''
             this.website = ''
-            this.stage = ''
+            this.industry = ''
+
             this.checkbox = null
             this.$refs.observer.reset()
         },
         send() {
             const data = {};
             data.dealName = this.dealName;
-            data.accountName = this.accountName;
+
+            data.dealOwner = this.dealOwner;
+            data.amount = this.amount;
             data.stage = this.stage;
+            data.closingDate = this.closingDate;
+            data.probability = this.probability;
+            data.dealDescription = this.dealDescription;
+
+
+            data.accountName = this.accountName;
+            data.accountOwner = this.accountOwner;
+            data.accountType = this.accountType;
+            data.billingAddress = this.billingAddress;
+            data.shippingAddress = this.shippingAddress;
             data.phone = this.phone;
             data.email = this.email;
             data.website = this.website;
+            data.industry = this.industry;
             axios.post('/account', data).then(function ({ data }){
                 console.log('data', data)
-            }).catch(function ({ rej }) {
+            }).catch(function (rej ) {
                 console.log('rej', rej)
-            }).finally(function ({ mes }){
+            }).finally(function (mes){
                 console.log('mes', mes)
             })
         }
