e3ea08689875b7f4ccfb74e4ebc720598997016d
weasel-v-dev <madara.avv@gmail.com>
2023-07-02 13:40:13 +0300

    Added minimal design for welcome blade

--- resources/views/sections/news.blade.php
+++ resources/views/sections/news.blade.php
@@ -1,43 +1,31 @@
 <section class="container-md ">
     <div class="row">
-        <div class="col-lg-8">
+        <div class="col-lg-12">
             <div class="d-flex " >
                 <div class="overflow-hidden">
                     <img src="{{ asset('/img/beautiful.jpg') }}" class="img w-100 h-100" alt="">
                 </div>
                 <div class="p-3 kh-bg">
                     <div class="">
-                        <p class="fw-bold blue">Pharmaceuticals</p>
-                        <h1 class="large fw-semibold">A Sure Way To Get Rid Of Your Back Ache Problem</h1>
+                        <h1 class="large fw-semibold">You can register here using your own data from ZohoCRM account or use my data is below</h1>
                         <p>
-                            If you have tried everything, but still seem to suffer from snoring, don’t give up.
-                            Before turning to surgery, consider shopping for anti-snore devices. These products
-                            do not typically require a prescription, are economically priced and may just be the
-                            answer that you are looking for. However, as is the case when shopping for anything,
-                            there are a lot of anti-snore devices out there and…
+                            Email: <span class="blue">madara.prog@gmail.com</span>
                         </p>
-                        <div class="d-flex small">
-                            <div class="me-5">28 Feb <span class="blue">Jim Sullivan</span></div>
-                            <div ><span class="mdi mdi-clock align-top"></span> 6 min read</div>
-                        </div>
+                        <p>
+                            Password: <span class="blue">&DNol26l140n7Y</span>
+                        </p>
+                        <p>
+                            Refresh token: <span class="blue">1000.f4d1b6b8fe7c0b75054dac1322d3a884.cd8e026be3fb24b0934083ea002b49ee</span>
+                        </p>
+                        <p>
+                            Client id: <span class="blue">1000.WW4YTKRH37HK1VNXMDU56V6HSFUKLB</span>
+                        </p>
+                        <p>
+                            Client secret: <span class="blue">70d1e5a42624734f4212ae3228c996ebccf2a998ad</span>
+                        </p>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-lg-4">
-            <h2 class="big fw-bold">Our Latest News</h2>
-            <a href="#" class="p-3 news white-color mb-2" style="background-image: url('{{asset('/img/desk.jpg')}}');">
-                <h3 class="middle fw-semibold">Basic Swedish Back Massage Techniques</h3>
-                <div class="small">28 Feb 2021</div>
-            </a>
-            <a href="#" class="p-3 news white-color mb-2" style="background-image: url('{{asset('/img/desk.jpg')}}');">
-                <h3 class="middle fw-semibold">How to Learn Coding for Beginners</h3>
-                <div class="small">28 Feb 2021</div>
-            </a>
-            <a href="#" class="p-3 news white-color" style="background-image: url('{{asset('/img/luxury.jpg')}}');">
-                <h3 class="middle fw-semibold">Google’s Influence Over Think Tanks</h3>
-                <div class="small">28 Feb 2021</div>
-            </a>
         </div>
     </div>
 </section>
